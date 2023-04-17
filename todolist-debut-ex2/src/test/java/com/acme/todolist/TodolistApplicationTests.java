ade9599e4605d050a7f73b46e9522cb9b5d3f50d
Florian LASSALLE <florian.lassalle@visiativ.com>
2023-04-17 19:29:27 +0200

    ex 3

--- todolist-debut-ex2/src/test/java/com/acme/todolist/TodolistApplicationTests.java
+++ todolist-debut-ex2/src/test/java/com/acme/todolist/TodolistApplicationTests.java
@@ -1,13 +1,31 @@
 package com.acme.todolist;
 
+import com.acme.todolist.domain.TodoItem;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 
-@SpringBootTest
+@SpringBootTest(classes = TodoItem.class)
 class TodolistApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	//A compléter
+
+	@Test
+	void testLate(){
+		TodoItem todoItem = new TodoItem("TU1", Instant.now().minus(2, ChronoUnit.DAYS), "You're late");
+		Assertions.assertEquals("[LATE!] You're late", todoItem.finalContent());
+	}
+
+	@Test
+	void testNotLate(){
+		TodoItem todoItem = new TodoItem("TU2", Instant.now(), "A wizard is never late, nor is he early, he arrives precisely when he means to.");
+		Assertions.assertEquals("A wizard is never late, nor is he early, he arrives precisely when he means to.", todoItem.finalContent());
 	}
 
 }
